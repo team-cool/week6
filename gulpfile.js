b0ad9e84a6d83a411a8bd43e16580adc1d3a2198
Vladimir Baranov <vbaranov@lowerprofile.comgit config --global user.email vbaranov@lowerprofile.comgit config --global user.email vbaranov@lowerprofile.com>
2016-08-09 16:36:25 -0400

    Gulp added

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+
+gulp.task('styles',function() {
+	return gulp.src('./css/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(concat('./css/main.css'))
+		.pipe(gulp.dest('.'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./css/**/*.scss', ['styles']);
+});
