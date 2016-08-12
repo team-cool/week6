f5f809fdd471ae42fef462c4ab0183e8c5b95ed3
Leah Conway <leahsconway@gmail.com>
2016-08-12 17:45:38 -0400

    main page styles

--- js/myApp.js
+++ js/myApp.js
@@ -214,6 +214,46 @@ myApp.updateResultBoxes = function(whichBook, whichMovie) {
 	});
 };
 
+//Flipping cards on main page
+
+(function() {
+
+  // cache vars
+  var cards = document.querySelectorAll(".card.effect__random");
+  var timeMin = 1;
+  var timeMax = 3;
+  var timeouts = [];
+
+  // loop through cards
+  for ( var i = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    var cardID = card.getAttribute("data-id");
+    var id = "timeoutID" + cardID;
+    var time = randomNum( timeMin, timeMax ) * 1000;
+    cardsTimeout( id, time, card );
+  }
+
+  // timeout listener
+  function cardsTimeout( id, time, card ) {
+    if (id in timeouts) {
+      clearTimeout(timeouts[id]);
+    }
+    timeouts[id] = setTimeout( function() {
+      var c = card.classList;
+      var newTime = randomNum( timeMin, timeMax ) * 1000;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+      cardsTimeout( id, newTime, card );
+    }, time );
+  }
+
+  // random number generator given min and max
+  function randomNum( min, max ) {
+    return Math.random() * (max - min) + min;
+  }
+
+})();
+
+
 // ****
 // INIT
 // ****
